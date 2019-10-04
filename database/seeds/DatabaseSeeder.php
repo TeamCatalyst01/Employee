bbc684eb5ca61fc5ab2bb45237b0e555343773ff
Jaskirat Singh <jaskiratsingh4242@gmail.com>
2019-10-04 16:58:02 +0530

    Removed Unwanted Seeds and updated Position Model file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(GendersTableSeeder::class);
          $this->call(AdminsTableSeeder::class);
     }
 }
