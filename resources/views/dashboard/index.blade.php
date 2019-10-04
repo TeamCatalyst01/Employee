9c9df86b71139ca4724a257c1171600d166f397c
upinder <upinderjits@gmail.com>
2019-10-04 10:49:11 +0530

    deleted on dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -42,33 +42,7 @@
                     </div>
                 </div>
             </a>
-            <a href="/cities" class="white-text hide-on-small-only">
-                <div class="mx-20 card-panel purple lighten-1 col s8 offset-s2 m4 l4 xl2">
-                    <div class="row">
-                        <div class="col s7 xl7">
-                            <i class="material-icons medium white-text pt-5">location_city</i>
-                            <h6 class="no-padding txt-md">Cities</h6>
-                        </div>
-                        <div class="col s5 xl5">
-                            <p class="no-padding center mt txt-sm">Total({{$t_cities}})</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="/countries" class="white-text">
-                <div class="mx-20 card-panel light-blue col s8 offset-s2 m4 offset-m2 l4 offset-l2 xl2 offset-xl1 ml-14">
-                    <div class="row">
-                        <div class="col s7 xl7">
-                            <i class="material-icons medium white-text pt-5">terrain</i>
-                            <h6 class="no-padding txt-md">Countries</h6>
-                        </div>
-                        <div class="col s5 xl5">
-                            <p class="no-padding center mt txt-sm">Total({{$t_countries}})</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="/salaries" class="white-text hide-on-small-only">
+3            <a href="/salaries" class="white-text hide-on-small-only">
                 <div class="card-panel green col s8 offset-s2 m4 l4 xl2 mx-20">
                     <div class="row">
                         <div class="col s7 xl7">
@@ -86,7 +60,7 @@
                     <div class="row">
                         <div class="col s7 xl7">
                             <i class="material-icons medium white-text pt-5">grid_on</i>
-                            <h6 class="no-padding txt-md">States</h6>
+                            <h6 class="no-padding txt-md">Payroll</h6>
                         </div>
                         <div class="col s5 xl5">
                             <p class="no-padding center mt txt-sm">Total({{$t_states}})</p>
